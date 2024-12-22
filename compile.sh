25cd485a24ef578293c66b28f512ab52c55ff240
Erlang Parasu <erlangparasu@gmail.com>
2024-12-23 00:28:55 +0800

    feat: script compile.sh

--- compile.sh
+++ compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
-rsync -avz --checksum ~/Downloads/lily58_r2g_layout_erlangparasu.json .
-rsync -avz lily58_r2g_layout_erlangparasu.json keyboards/lily58/keymaps/default/
-qmk json2c keyboards/lily58/keymaps/default/lily58_r2g_layout_erlangparasu.json > keyboards/lily58/keymaps/default/generated_keymap.c
+rsync -avz ~/Downloads/lily58_rev1_mouse_erlangparasu.json keyboards/lily58/keymaps/default/
+qmk json2c ~/Downloads/lily58_rev1_mouse_erlangparasu.json > keyboards/lily58/keymaps/default/generated_keymap.c
 qmk compile -kb lily58/rev1 -km default
 # eof
