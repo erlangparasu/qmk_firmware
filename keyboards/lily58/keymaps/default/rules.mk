9ad5df7185792d37ce041ccc396eb6533e86043e
Erlang Parasu <erlangparasu@gmail.com>
2024-12-22 11:16:51 +0800

    feat: enable oled, wpm, include keymap mouse

--- keyboards/lily58/keymaps/default/rules.mk
+++ keyboards/lily58/keymaps/default/rules.mk
@@ -9,8 +9,11 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
-OLED_ENABLE = no           # OLED display
-WPM_ENABLE = no
+OLED_ENABLE = yes           # OLED display
+WPM_ENABLE = yes
+
+SHARED_EP_ENABLE = yes
+KEYBOARD_SHARED_EP = yes
 
 # If you want to change the display of OLED, you need to change here
 SRC += ./lib/rgb_state_reader.c \
